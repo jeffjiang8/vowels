748d80a78c37bcd70dfd01d3b5ac6c8f4a4f6e04
Jeff Jiang <jeffjiang8@gmail.com>
2020-04-23 19:30:12 -0400

    2.0

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ function vowels(str) {
         }
     }
     return count
+
+    // const matches = str.match(/[aeiou]/gi)
+    // // g => make sure the function doesn't stop after finding the first one, i => insensitive
+    // // match will return 'null' or an array of matches
+    // return matches ? matches.length : 0
 }
